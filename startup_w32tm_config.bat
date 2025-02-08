01a87dd28dcb11f788ee78328808d0559258bb98
Stream <kvantarium@gmail.com>
2025-02-08 17:54:31 +0200

    Update startup_w32tm_config.bat

--- startup_w32tm_config.bat
+++ startup_w32tm_config.bat
@@ -10,6 +10,8 @@ net stop w32time
 rem "Allow large offset sync"
 reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\w32time\Config /v MaxNegPhaseCorrection /d 0xFFFFFFFF /t REG_DWORD /f
 reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\w32time\Config /v MaxPosPhaseCorrection /d 0xFFFFFFFF /t REG_DWORD /f
+rem "set sync interval to 30min"
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\W32Time\TimeProviders\NtpClient /v SpecialPollInterval /d 0x00000708 /t REG_DWORD /f
 
 net start w32time
 
